9a22e092bdf2c2747a45d938c28e155217c0a43d
Richard Howes <richardh@workflowgroup.com>
2023-05-04 21:50:50 +0000

    implement eloquent-api-resources-transform-fields

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Http\Resources\PostResource;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return Post::all();
+        return PostResource::collection(Post::all());
     }
 }
