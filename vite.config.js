20dbc0823a9b235113a14e5c540b8bad7696de01
Richard Howes <richardh@workflowgroup.com>
2023-05-04 21:47:27 +0000

    implement load-data-vue-composition-api

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue'; 
- 
+import vue from '@vitejs/plugin-vue';
+
 export default defineConfig({
     plugins: [
         laravel({
@@ -11,7 +11,7 @@ export default defineConfig({
             ],
             refresh: true,
         }),
-        vue({ 
+        vue({
             template: {
                 transformAssetUrls: {
                     base: null,
@@ -23,6 +23,7 @@ export default defineConfig({
     resolve: {
         alias: {
             vue: 'vue/dist/vue.esm-bundler.js',
+            '@': '/resources/js', 
         },
-    }, 
+    },
 });
